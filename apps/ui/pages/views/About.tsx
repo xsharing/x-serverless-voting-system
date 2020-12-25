4023e733b98d19733bcbfc7a5f4058ddb01e96a3
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2020-12-25 20:05:00 +0900

    introduce material ui

--- apps/ui/pages/views/About.tsx
+++ apps/ui/pages/views/About.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import * as React from 'react';
+import { Button } from '@material-ui/core';
 
 const AboutPage: NextPage = () => (
   <div>
     <p>about us!!</p>
+    <Link href="/">
+      <Button variant="contained" color="primary">
+        material ui button
+      </Button>
+    </Link>
   </div>
 );
 
