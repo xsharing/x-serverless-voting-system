f0f98c4cb9b2cd779b1f251e3f34b54bf0ea4aaa
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2021-01-03 00:00:00 +0900

    react json schema formを導入してみる

--- apps/user-web/src/ui/pages/views/CampaignDetail.tsx
+++ apps/user-web/src/ui/pages/views/CampaignDetail.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Checkbox,
   Container,
   FormControl,
@@ -13,6 +14,8 @@ import {
 import { NextPage } from 'next';
 import Link from 'next/link';
 import * as React from 'react';
+import RJSForm from '@rjsf/material-ui';
+import { JSONSchema7 } from 'json-schema';
 
 const useStyles = makeStyles((theme) => ({
   form: {},
@@ -20,6 +23,14 @@ const useStyles = makeStyles((theme) => ({
 
 const IndexPage: NextPage = () => {
   const classes = useStyles();
+
+  const schema: JSONSchema7 = {
+    title: 'Custom array of strings',
+    type: 'array',
+    items: {
+      type: 'string',
+    },
+  };
 
   const [checks, setChecks] = React.useState([false, false, false]);
 
@@ -32,6 +43,10 @@ const IndexPage: NextPage = () => {
     console.log(newChecks);
   };
 
+  const handleVote = (event) => {
+    console.log(event);
+  };
+
   const error = checks.filter((c) => c).length === 2 ? false : true;
 
   return (
@@ -39,44 +54,51 @@ const IndexPage: NextPage = () => {
       <Paper>
         <form className="classes.form" autoComplete="off">
           <TextField id="code" label="投票コード" fullWidth />
+          <RJSForm schema={schema}></RJSForm>
+          <FormControl required error={error} component="fieldset">
+            <FormLabel component="legend">2つ選択してください。</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checks[0]}
+                    onChange={handleChange}
+                    name="0"
+                  />
+                }
+                label="Gilad Gray"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checks[1]}
+                    onChange={handleChange}
+                    name="1"
+                  />
+                }
+                label="Jason Killian"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checks[2]}
+                    onChange={handleChange}
+                    name="2"
+                  />
+                }
+                label="Antoine Llorca"
+              />
+            </FormGroup>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleVote}
+              disabled={error}
+            >
+              投票{error ? '(エラー)' : ''}
+            </Button>
+          </FormControl>
         </form>
-
-        <FormControl required error={error} component="fieldset">
-          <FormLabel component="legend">2つ選択してください。</FormLabel>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checks[0]}
-                  onChange={handleChange}
-                  name="0"
-                />
-              }
-              label="Gilad Gray"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checks[1]}
-                  onChange={handleChange}
-                  name="1"
-                />
-              }
-              label="Jason Killian"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checks[2]}
-                  onChange={handleChange}
-                  name="2"
-                />
-              }
-              label="Antoine Llorca"
-            />
-          </FormGroup>
-          {/* <FormHelperText>You can display an error</FormHelperText> */}
-        </FormControl>
       </Paper>
     </Container>
   );
