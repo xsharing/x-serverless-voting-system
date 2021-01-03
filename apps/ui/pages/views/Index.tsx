bfeef6a79a5157f729c80d06b1aaa2dbcc766864
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2021-01-03 15:32:34 +0900

    material ui ssr

--- apps/ui/pages/views/Index.tsx
+++ apps/ui/pages/views/Index.tsx
@@ -1,16 +1,34 @@
+import { Container, CssBaseline, makeStyles } from '@material-ui/core';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import * as React from 'react';
 
-const IndexPage: NextPage = () => (
-  <div>
-    <p>
-      Hello
-      <Link href="/about">
-        <a>About us</a>
-      </Link>
-    </p>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    borderWidth: '1px',
+    borderColor: 'black',
+    borderStyle: 'solid',
+  },
+}));
+
+const IndexPage: NextPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <div className={classes.form}>Hello</div>
+      </div>
+    </Container>
+  );
+};
 
 export default IndexPage;
